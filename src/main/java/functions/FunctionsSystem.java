e1134e7299afea322c31eff776b9ccd7277d4b12
Ekaterina Kubikova <48138888+Kratlis@users.noreply.github.com>
2021-04-24 18:19:55 +0300

    changed variable names

--- src/main/java/functions/FunctionsSystem.java
+++ src/main/java/functions/FunctionsSystem.java
@@ -2,22 +2,22 @@ package functions;
 
 public class FunctionsSystem extends Function {
 
-    private final Function function1;
-    private final Function function2;
+    private final Function trigonometryFunction;
+    private final Function logFunction;
 
-    public FunctionsSystem(double precision, Function function1, Function function2) {
+    public FunctionsSystem(double precision, Function trigonometryFunction, Function logFunction) {
         super(precision);
 
-        this.function1 = function1;
-        this.function2 = function2;
+        this.trigonometryFunction = trigonometryFunction;
+        this.logFunction = logFunction;
     }
 
     @Override
     public Double calculate(double x) {
         if (x <= 0) {
-            return function1.calculate(x);
+            return trigonometryFunction.calculate(x);
         } else {
-            return function2.calculate(x);
+            return logFunction.calculate(x);
         }
     }
 
