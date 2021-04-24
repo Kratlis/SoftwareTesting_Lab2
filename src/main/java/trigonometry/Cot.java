f37784c52a8f6788f6bffb74f8b4db175fe331e3
Ekaterina Kubikova <48138888+Kratlis@users.noreply.github.com>
2021-04-24 18:20:31 +0300

    added setters

--- src/main/java/trigonometry/Cot.java
+++ src/main/java/trigonometry/Cot.java
@@ -3,15 +3,23 @@ package trigonometry;
 import functions.Function;
 
 public class Cot extends Function {
-    private final Sin sinFunction = new Sin(getPrecision());
-    private final Cos cosFunction = new Cos(getPrecision());
+    private Sin sinFunction = new Sin(getPrecision());
+    private Cos cosFunction = new Cos(getPrecision());
 
-    protected Cot(double precision) {
+    public Cot(double precision) {
         super(precision);
     }
 
     @Override
     public Double calculate(double x) {
         return cosFunction.calculate(x) / sinFunction.calculate(x);
+    }
+
+    public void setSin(Sin sinFunction) {
+        this.sinFunction = sinFunction;
+    }
+
+    public void setCos(Cos cosFunction) {
+        this.cosFunction = cosFunction;
     }
 }
--- src/main/java/trigonometry/Tan.java
+++ src/main/java/trigonometry/Tan.java
@@ -3,15 +3,23 @@ package trigonometry;
 import functions.Function;
 
 public class Tan extends Function {
-    private final Sin sinFunction = new Sin(getPrecision());
-    private final Cos cosFunction = new Cos(getPrecision());
+    private Sin sinFunction = new Sin(getPrecision());
+    private Cos cosFunction = new Cos(getPrecision());
 
-    protected Tan(double precision) {
+    public Tan(double precision) {
         super(precision);
     }
 
     @Override
     public Double calculate(double x) {
         return sinFunction.calculate(x) / cosFunction.calculate(x);
+    }
+
+    public void setSin(Sin sinFunction) {
+        this.sinFunction = sinFunction;
+    }
+
+    public void setCos(Cos cosFunction) {
+        this.cosFunction = cosFunction;
     }
 }
