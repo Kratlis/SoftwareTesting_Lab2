2b07549aa96215ad867d7d5e9d0ff25ea07d9658
Ekaterina Kubikova <48138888+Kratlis@users.noreply.github.com>
2021-04-24 18:24:42 +0300

    changed mocks

--- src/test/java/SystemMocks.java
+++ src/test/java/SystemMocks.java
@@ -2,7 +2,6 @@ import log.LogFunction;
 import org.mockito.AdditionalMatchers;
 import trigonometry.TrigonometryFunction;
 
-import static java.lang.Double.NaN;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -11,12 +10,12 @@ public class SystemMocks {
     public static LogFunction getLogFunctionMock() {
         LogFunction logFunction = mock(LogFunction.class);
 
-        when(logFunction.calculate(AdditionalMatchers.eq(0.0, 0.1))).thenReturn(NaN);
-        when(logFunction.calculate(AdditionalMatchers.eq(0.5, 0.1))).thenReturn(-0.946261);
-        when(logFunction.calculate(AdditionalMatchers.eq(0.8, 0.1))).thenReturn(-0.088937);
+        when(logFunction.calculate(AdditionalMatchers.eq(0.2, 0.1))).thenReturn(72.8924);
+        when(logFunction.calculate(AdditionalMatchers.eq(0.5, 0.1))).thenReturn(0.002968);
+        when(logFunction.calculate(AdditionalMatchers.eq(0.8, 0.1))).thenReturn(0.0000000037);
         when(logFunction.calculate(AdditionalMatchers.eq(1.0, 0.1))).thenReturn(0.0);
-        when(logFunction.calculate(AdditionalMatchers.eq(1.2, 0.1))).thenReturn(-0.054115);
-        when(logFunction.calculate(AdditionalMatchers.eq(1.5, 0.1))).thenReturn(-0.253324);
+        when(logFunction.calculate(AdditionalMatchers.eq(1.2, 0.1))).thenReturn(0.0000000003);
+        when(logFunction.calculate(AdditionalMatchers.eq(1.5, 0.1))).thenReturn(0.00000476);
 
         return logFunction;
     }
@@ -24,17 +23,13 @@ public class SystemMocks {
     public static TrigonometryFunction getTrigonometryFunctionMock() {
         TrigonometryFunction trigonometryFunction = mock(TrigonometryFunction.class);
 
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-3.1415927, 0.1))).thenReturn(NaN);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-2.6179939, 0.1))).thenReturn(1.7320508);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-2.3561945, 0.1))).thenReturn(1.0);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-1.5707963, 0.1))).thenReturn(0.0);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-0.7853982, 0.1))).thenReturn(-1.0);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-0.5235988, 0.1))).thenReturn(-1.7320508);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(0.0, 0.1))).thenReturn(NaN);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(0.7853982, 0.1))).thenReturn(1.0);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(1.5707963, 0.1))).thenReturn(0.0);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(2.3561945, 0.1))).thenReturn(-1.0);
-        when(trigonometryFunction.calculate(AdditionalMatchers.eq(3.1415927, 0.1))).thenReturn(NaN);
+        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-3.1415927, 0.1))).thenReturn(0.0);
+        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-2.6179939, 0.1))).thenReturn(-0.5463);
+        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-2.3561945, 0.1))).thenReturn(-0.85451);
+        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-1.5707963, 0.1))).thenReturn(-1.5574);
+        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-0.7853982, 0.1))).thenReturn(-0.85451);
+        when(trigonometryFunction.calculate(AdditionalMatchers.eq(-0.5235988, 0.1))).thenReturn(-0.5463);
+        when(trigonometryFunction.calculate(AdditionalMatchers.eq(0.0, 0.1))).thenReturn(0.0);
 
         return trigonometryFunction;
     }
--- src/test/java/trigonometryTests/TrigonometryMocks.java
+++ src/test/java/trigonometryTests/TrigonometryMocks.java
@@ -1,11 +1,9 @@
 package trigonometryTests;
 
 import org.mockito.AdditionalMatchers;
-import trigonometry.Cos;
-import trigonometry.Csc;
-import trigonometry.Sin;
+import trigonometry.*;
 
-import static java.lang.Double.NaN;
+import static java.lang.Double.*;
 import static java.lang.Math.PI;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -19,7 +17,7 @@ public class TrigonometryMocks {
         when(cos.calculate(AdditionalMatchers.eq(0.5235988, 0.1))).thenReturn(0.8660254);
         when(cos.calculate(AdditionalMatchers.eq(0.7853982, 0.1))).thenReturn(0.7071068);
         when(cos.calculate(AdditionalMatchers.eq(1.0471976, 0.1))).thenReturn(0.5);
-        when(cos.calculate(AdditionalMatchers.eq(1.5707963, 0.1))).thenReturn(0.0);
+        when(cos.calculate(AdditionalMatchers.eq(1.5707963, 0.1))).thenReturn(0.00000002679);
         when(cos.calculate(AdditionalMatchers.eq(2.0943951, 0.1))).thenReturn(-0.5);
         when(cos.calculate(AdditionalMatchers.eq(2.3561945, 0.1))).thenReturn(-0.7071068);
         when(cos.calculate(AdditionalMatchers.eq(2.6179939, 0.1))).thenReturn(-0.8660254);
@@ -55,46 +53,6 @@ public class TrigonometryMocks {
         return cos;
     }
 
-    public static Csc getCscMock() {
-        Csc csc = mock(Csc.class);
-
-        when(csc.calculate(AdditionalMatchers.eq(0.0, 0.1))).thenReturn(NaN);
-        when(csc.calculate(AdditionalMatchers.eq(0.5235988, 0.1))).thenReturn(2.0);
-        when(csc.calculate(AdditionalMatchers.eq(0.7853982, 0.1))).thenReturn(1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(1.0471976, 0.1))).thenReturn(1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(1.5707963, 0.1))).thenReturn(1.0);
-        when(csc.calculate(AdditionalMatchers.eq(2.0943951, 0.1))).thenReturn(1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(2.3561945, 0.1))).thenReturn(1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(2.6179939, 0.1))).thenReturn(2.0);
-        when(csc.calculate(AdditionalMatchers.eq(3.1415927, 0.1))).thenReturn(NaN);
-        when(csc.calculate(AdditionalMatchers.eq(3.6651914, 0.1))).thenReturn(-2.0);
-        when(csc.calculate(AdditionalMatchers.eq(3.9269908, 0.1))).thenReturn(-1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(4.1887902, 0.1))).thenReturn(-1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(4.7123890, 0.1))).thenReturn(-1.0);
-        when(csc.calculate(AdditionalMatchers.eq(5.2359878, 0.1))).thenReturn(-1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(5.4977871, 0.1))).thenReturn(-1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(5.7595865, 0.1))).thenReturn(-2.0);
-        when(csc.calculate(AdditionalMatchers.eq(6.2831853, 0.1))).thenReturn(NaN);
-        when(csc.calculate(AdditionalMatchers.eq(-0.5235988, 0.1))).thenReturn(-2.0);
-        when(csc.calculate(AdditionalMatchers.eq(-0.7853982, 0.1))).thenReturn(-1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(-1.0471976, 0.1))).thenReturn(-1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(-1.5707963, 0.1))).thenReturn(-1.0);
-        when(csc.calculate(AdditionalMatchers.eq(-2.0943951, 0.1))).thenReturn(-1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(-2.3561945, 0.1))).thenReturn(-1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(-2.6179939, 0.1))).thenReturn(-2.0);
-        when(csc.calculate(AdditionalMatchers.eq(-3.1415927, 0.1))).thenReturn(NaN);
-        when(csc.calculate(AdditionalMatchers.eq(-3.6651914, 0.1))).thenReturn(2.0);
-        when(csc.calculate(AdditionalMatchers.eq(-3.9269908, 0.1))).thenReturn(1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(-4.1887902, 0.1))).thenReturn(1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(-4.7123890, 0.1))).thenReturn(1.0);
-        when(csc.calculate(AdditionalMatchers.eq(-5.2359878, 0.1))).thenReturn(1.1547005);
-        when(csc.calculate(AdditionalMatchers.eq(-5.4977871, 0.1))).thenReturn(1.4142136);
-        when(csc.calculate(AdditionalMatchers.eq(-5.7595865, 0.1))).thenReturn(2.0);
-        when(csc.calculate(AdditionalMatchers.eq(-6.2831853, 0.1))).thenReturn(NaN);
-        when(csc.calculate(NaN)).thenThrow(IllegalArgumentException.class);
-        return csc;
-    }
-
     public static Sin getSinMock() {
         Sin sin = mock(Sin.class);
 
@@ -106,7 +64,7 @@ public class TrigonometryMocks {
         when(sin.calculate(AdditionalMatchers.eq(2.0943951, 0.1))).thenReturn(0.866);
         when(sin.calculate(AdditionalMatchers.eq(2.3561945, 0.1))).thenReturn(0.7071);
         when(sin.calculate(AdditionalMatchers.eq(2.6179939, 0.1))).thenReturn(0.49999998);
-        when(sin.calculate(AdditionalMatchers.eq(3.1415927, 0.1))).thenReturn(0.0);
+        when(sin.calculate(AdditionalMatchers.eq(3.1415927, 0.1))).thenReturn(-0.00000004641);
         when(sin.calculate(AdditionalMatchers.eq(3.6651914, 0.1))).thenReturn(-0.49999997);
         when(sin.calculate(AdditionalMatchers.eq(3.9269908, 0.1))).thenReturn(-0.7071);
         when(sin.calculate(AdditionalMatchers.eq(4.1887902, 0.1))).thenReturn(-0.866);
@@ -153,5 +111,96 @@ public class TrigonometryMocks {
         when(sin.calculate(Double.NEGATIVE_INFINITY)).thenReturn(NaN);
         when(sin.calculate(NaN)).thenThrow(IllegalArgumentException.class);
         return sin;
+    }
+
+    public static Csc getCscMock() {
+        Csc csc = mock(Csc.class);
+
+        when(csc.calculate(AdditionalMatchers.eq(0.5235988, 0.1))).thenReturn(2.0);
+        when(csc.calculate(AdditionalMatchers.eq(0.7853982, 0.1))).thenReturn(1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(1.0471976, 0.1))).thenReturn(1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(1.5707963, 0.1))).thenReturn(1.0);
+        when(csc.calculate(AdditionalMatchers.eq(2.0943951, 0.1))).thenReturn(1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(2.3561945, 0.1))).thenReturn(1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(2.6179939, 0.1))).thenReturn(2.0);
+        when(csc.calculate(AdditionalMatchers.eq(3.1415927, 0.1))).thenReturn(-21546984.376);
+        when(csc.calculate(AdditionalMatchers.eq(3.6651914, 0.1))).thenReturn(-2.0);
+        when(csc.calculate(AdditionalMatchers.eq(3.9269908, 0.1))).thenReturn(-1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(4.1887902, 0.1))).thenReturn(-1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(4.7123890, 0.1))).thenReturn(-1.0);
+        when(csc.calculate(AdditionalMatchers.eq(5.2359878, 0.1))).thenReturn(-1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(5.4977871, 0.1))).thenReturn(-1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(5.7595865, 0.1))).thenReturn(-2.0);
+        when(csc.calculate(AdditionalMatchers.eq(6.2831853, 0.1))).thenReturn(0.137209);
+        when(csc.calculate(AdditionalMatchers.eq(-0.5235988, 0.1))).thenReturn(-2.0);
+        when(csc.calculate(AdditionalMatchers.eq(-0.7853982, 0.1))).thenReturn(-1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(-1.0471976, 0.1))).thenReturn(-1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(-1.5707963, 0.1))).thenReturn(-1.0);
+        when(csc.calculate(AdditionalMatchers.eq(-2.0943951, 0.1))).thenReturn(-1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(-2.3561945, 0.1))).thenReturn(-1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(-2.6179939, 0.1))).thenReturn(-2.0);
+        when(csc.calculate(AdditionalMatchers.eq(-3.1415927, 0.1))).thenReturn(21546984.376);
+        when(csc.calculate(AdditionalMatchers.eq(-3.6651914, 0.1))).thenReturn(2.0);
+        when(csc.calculate(AdditionalMatchers.eq(-3.9269908, 0.1))).thenReturn(1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(-4.1887902, 0.1))).thenReturn(1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(-4.7123890, 0.1))).thenReturn(1.0);
+        when(csc.calculate(AdditionalMatchers.eq(-5.2359878, 0.1))).thenReturn(1.1547005);
+        when(csc.calculate(AdditionalMatchers.eq(-5.4977871, 0.1))).thenReturn(1.4142136);
+        when(csc.calculate(AdditionalMatchers.eq(-5.7595865, 0.1))).thenReturn(2.0);
+        when(csc.calculate(AdditionalMatchers.eq(-6.2831853, 0.1))).thenReturn(139283787.89);
+        when(csc.calculate(NaN)).thenThrow(IllegalArgumentException.class);
+        return csc;
+    }
+
+    public static Sec getSecMock() {
+        Sec sec = mock(Sec.class);
+
+        when(sec.calculate(AdditionalMatchers.eq(-0.5235988, 0.1))).thenReturn(1.1547);
+        when(sec.calculate(AdditionalMatchers.eq(-0.7853982, 0.1))).thenReturn(1.4142136);
+        when(sec.calculate(AdditionalMatchers.eq(-1.0471976, 0.1))).thenReturn(2.0);
+        when(sec.calculate(AdditionalMatchers.eq(-1.5707963, 0.1))).thenReturn(37320539.59);
+        when(sec.calculate(AdditionalMatchers.eq(-2.0943951, 0.1))).thenReturn(-2.0);
+        when(sec.calculate(AdditionalMatchers.eq(-2.3561945, 0.1))).thenReturn(-1.4142136);
+        when(sec.calculate(AdditionalMatchers.eq(-2.6179939, 0.1))).thenReturn(-1.1547);
+        when(sec.calculate(AdditionalMatchers.eq(-3.1415927, 0.1))).thenReturn(-1.0);
+        when(sec.calculate(Double.POSITIVE_INFINITY)).thenReturn(NaN);
+        when(sec.calculate(Double.NEGATIVE_INFINITY)).thenReturn(NaN);
+        when(sec.calculate(NaN)).thenThrow(IllegalArgumentException.class);
+        return sec;
+    }
+
+    public static Cot getCotMock() {
+        Cot cot = mock(Cot.class);
+
+        when(cot.calculate(AdditionalMatchers.eq(-0.5235988, 0.1))).thenReturn(-1.732054);
+        when(cot.calculate(AdditionalMatchers.eq(-0.7853982, 0.1))).thenReturn(-1.0);
+        when(cot.calculate(AdditionalMatchers.eq(-1.0471976, 0.1))).thenReturn(-0.57735);
+        when(cot.calculate(AdditionalMatchers.eq(-1.5707963, 0.1))).thenReturn(0.0);
+        when(cot.calculate(AdditionalMatchers.eq(-2.0943951, 0.1))).thenReturn(0.57735);
+        when(cot.calculate(AdditionalMatchers.eq(-2.3561945, 0.1))).thenReturn(1.0);
+        when(cot.calculate(AdditionalMatchers.eq(-2.6179939, 0.1))).thenReturn(1.732054);
+        when(cot.calculate(AdditionalMatchers.eq(-3.1415927, 0.1))).thenReturn(-21546984.376);
+        when(cot.calculate(Double.POSITIVE_INFINITY)).thenReturn(NaN);
+        when(cot.calculate(Double.NEGATIVE_INFINITY)).thenReturn(NaN);
+        when(cot.calculate(NaN)).thenThrow(IllegalArgumentException.class);
+        return cot;
+    }
+
+    public static Tan getTanMock() {
+        Tan tan = mock(Tan.class);
+
+        when(tan.calculate(AdditionalMatchers.eq(0.0, 0.1))).thenReturn(0.0);
+        when(tan.calculate(AdditionalMatchers.eq(-0.5235988, 0.1))).thenReturn(-0.57735);
+        when(tan.calculate(AdditionalMatchers.eq(-0.7853982, 0.1))).thenReturn(-1.0);
+        when(tan.calculate(AdditionalMatchers.eq(-1.0471976, 0.1))).thenReturn(-1.73205);
+        when(tan.calculate(AdditionalMatchers.eq(-1.5707963, 0.1))).thenReturn(-37320539.5867);
+        when(tan.calculate(AdditionalMatchers.eq(-2.0943951, 0.1))).thenReturn(1.73205);
+        when(tan.calculate(AdditionalMatchers.eq(-2.3561945, 0.1))).thenReturn(1.0);
+        when(tan.calculate(AdditionalMatchers.eq(-2.6179939, 0.1))).thenReturn(0.57735);
+        when(tan.calculate(AdditionalMatchers.eq(-3.1415927, 0.1))).thenReturn(0.0);
+        when(tan.calculate(Double.POSITIVE_INFINITY)).thenReturn(NaN);
+        when(tan.calculate(Double.NEGATIVE_INFINITY)).thenReturn(NaN);
+        when(tan.calculate(NaN)).thenThrow(IllegalArgumentException.class);
+        return tan;
     }
 }
