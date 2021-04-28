4d9cdbb2fd58fd8c14a0f1e76f6d3a98a8b418db
Ekaterina Kubikova <48138888+Kratlis@users.noreply.github.com>
2021-04-28 11:24:19 +0300

    added tests

--- src/test/java/trigonometryTests/CotTest.java
+++ src/test/java/trigonometryTests/CotTest.java
@@ -1,4 +1,62 @@
 package trigonometryTests;
 
+import instruments.CsvLogger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import trigonometry.Cot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(value = PER_CLASS)
 public class CotTest {
+    private static final double DELTA = 0.05;
+    private static final double ACCURACY = 0.0001;
+
+    private Cot cot;
+
+    @BeforeAll
+    public void init() {
+        cot = new Cot(ACCURACY);
+        cot.setCos(TrigonometryMocks.getCosMock());
+        cot.setSin(TrigonometryMocks.getSinMock());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/cot_test_data.csv")
+    public void tableValuesTest(float x, float expected) {
+        double actual = cot.calculate(x);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @AfterAll
+    public void logInCSV() {
+        CsvLogger csvLogger = new CsvLogger("cot.csv", -5, -1, 0.1);
+        csvLogger.log(cot);
+    }
+
+    @Test
+    public void nanTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> cot.calculate(Double.NaN));
+    }
+
+    @Test
+    public void positiveInfinityTest() {
+        double expected = Double.NaN;
+        double actual = cot.calculate(Double.POSITIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void negativeInfinityTest() {
+        double expected = Double.NaN;
+        double actual = cot.calculate(Double.NEGATIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
 }
--- src/test/java/trigonometryTests/TanTest.java
+++ src/test/java/trigonometryTests/TanTest.java
@@ -1,4 +1,62 @@
 package trigonometryTests;
 
+import instruments.CsvLogger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import trigonometry.Tan;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(value = PER_CLASS)
 public class TanTest {
+    private static final double DELTA = 0.05;
+    private static final double ACCURACY = 0.0001;
+
+    private Tan tan;
+
+    @BeforeAll
+    public void init() {
+        tan = new Tan(ACCURACY);
+        tan.setCos(TrigonometryMocks.getCosMock());
+        tan.setSin(TrigonometryMocks.getSinMock());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/tan_test_data.csv")
+    public void tableValuesTest(float x, float expected) {
+        double actual = tan.calculate(x);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @AfterAll
+    public void logInCSV() {
+        CsvLogger csvLogger = new CsvLogger("tan.csv", -5, -1, 0.1);
+        csvLogger.log(tan);
+    }
+
+    @Test
+    public void nanTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> tan.calculate(Double.NaN));
+    }
+
+    @Test
+    public void positiveInfinityTest() {
+        double expected = Double.NaN;
+        double actual = tan.calculate(Double.POSITIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void negativeInfinityTest() {
+        double expected = Double.NaN;
+        double actual = tan.calculate(Double.NEGATIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
 }
